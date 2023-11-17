e274384431e2a5790a352c9e543f46bb86b0dbf9
LeeHueeng <zzxx373014@gmail.com>
2023-11-18 04:51:18 +0900

    feat #2 - navbar 기본 틀 구현
    navbar 데스크탑의 기본형태 잡아둠
    position, shadow, font 등

--- portefolio/src/Component/Navbar.js
+++ portefolio/src/Component/Navbar.js
@@ -1,16 +1,47 @@
 import React from 'react'
-import { css } from 'styled-components'
+import styled from 'styled-components'
 
 export default function Navbar() {
     return (
-        <div css='Container'>
-            <div>d</div>
-        </div>
+        <Container>
+            <div className='CenterFlex'>
+                <button>Home</button>
+                <button>Home</button>
+                <button>Home</button>
+                <button>Home</button>
+                <button>Home</button>
+            </div>
+        </Container>
     )
 }
 
-const Container = css`
+const Container = styled.div`
+    position: fixed;
+    top:0;
     width:100%;
-    background-color:red;
-    color : blue;
+    height:10vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #DFA67B;
+    .CenterFlex{
+       
+    }
+    button{
+        width: 100px;
+        height: 50px;
+        border-radius: 1rem;
+        border: none;
+        background-color: #FFD966s;
+        margin: 0 20px;
+        padding: 0 20px;
+        font-size: 1.5rem;
+        font-weight: bold;
+        color: #DFA67B;
+        cursor: pointer;
+        box-shadow: 0 0 10px 0 rgba(0,0,0,0.5);
+        &:hover{
+            background-color: #FFF2CC;
+        }
+    }
 `
