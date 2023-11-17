ebcb2b9ed6b5e490141d51ed5d6036b1d1084463
LeeHueeng <zzxx373014@gmail.com>
2023-11-18 04:02:01 +0900

    feat #1 - styled-component 패키지 추가

---
+++ portefolio/src/Component/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { css } from 'styled-components'
+
+export default function Navbar() {
+    return (
+        <div css='Container'>
+            <div>d</div>
+        </div>
+    )
+}
+
+const Container = css`
+    width:100%;
+    background-color:red;
+    color : blue;
+`
