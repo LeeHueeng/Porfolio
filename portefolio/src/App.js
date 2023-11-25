79c1c1b694d5b5b69d59c9f8a571ad3c344d2d24
LeeHueeng <zzxx373014@gmail.com>
2023-11-26 06:11:42 +0900

    feat #5 - Work컴포넌트 생성 및 적용

--- portefolio/src/App.js
+++ portefolio/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import About from './Components/About';
+import Work from './Components/Work';
 
 function App() {
   return (
     <div >
-
       <About />
+      <Work />
     </div>
   );
 }
