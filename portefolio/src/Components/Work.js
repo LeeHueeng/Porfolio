50be68c793c07e56e96586089067ad13d3f99bbe
LeeHueeng <zzxx373014@gmail.com>
2023-11-30 16:11:45 +0900

    feat #5 - 각 div 클릭시 slecet 값 조정

--- portefolio/src/Components/Work.js
+++ portefolio/src/Components/Work.js
@@ -1,71 +1,76 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components';
 
 export default function Work() {
+    const [select, setSelect] = useState(0);
+
+    useEffect(() => {
+        console.log(select);
+    }, [select])
     return (
         <Container>
             <div className='container'>
 
                 <div className='contents'>
                     <div className='firstcontent'>
-                        <div className='select'>
+                        <div className='select' onClick={() => setSelect(1)}>
                             <h1>hueeng 사이트</h1>
                             <img src='https://cdn.pixabay.com/photo/2016/11/29/05/45/astronomy-1867616_960_720.jpg' width={500} />
                         </div>
-                        <div className='select'>
+                        <div className='select' onClick={() => setSelect(2)}>
                             <h1>Giant Step 기획자 인턴</h1>
                             <img src='https://cdn.pixabay.com/photo/2016/11/29/05/45/astronomy-1867616_960_720.jpg' width={500} />
                         </div>
-                        <div className='select'>
+                        <div className='select' onClick={() => setSelect(3)}>
                             <h1>알고리즘 어울림</h1>
                             <img src='https://cdn.pixabay.com/photo/2016/11/29/05/45/astronomy-1867616_960_720.jpg' width={500} />
                         </div>
-                        <div className='select'>
+                        <div className='select' onClick={() => setSelect(4)}>
                             <h1>unity game 제작 </h1>
                             <img src='https://cdn.pixabay.com/photo/2016/11/29/05/45/astronomy-1867616_960_720.jpg' width={500} />
                         </div>
                     </div>
                     <div className='firstcontent'>
-                        <div className='select'>
+                        <div className='select' onClick={() => setSelect(5)}>
                             <h1>IT동아리 S.owl 활동</h1>
                             <img src='https://cdn.pixabay.com/photo/2016/11/29/05/45/astronomy-1867616_960_720.jpg' width={500} />
                         </div>
-                        <div className='select'>
+                        <div className='select' onClick={() => setSelect(6)}>
                             <h1>나만의 커뮤니티 제작</h1>
                             <img src='https://cdn.pixabay.com/photo/2016/11/29/05/45/astronomy-1867616_960_720.jpg' width={500} />
                         </div>
-                        <div className='select'>
+                        <div className='select' onClick={() => setSelect(7)}>
                             <h1>문서관리 웹사이트 제작(DMS)</h1>
                             <img src='https://cdn.pixabay.com/photo/2016/11/29/05/45/astronomy-1867616_960_720.jpg' width={500} />
                         </div>
-                        <div className='select'>
+                        <div className='select' onClick={() => setSelect(8)}>
                             <h1>IT 경진대회 대상</h1>
                             <img src='https://cdn.pixabay.com/photo/2016/11/29/05/45/astronomy-1867616_960_720.jpg' width={500} />
                         </div>
                     </div>
                     <div className='firstcontent'>
-                        <div className='select'>
+                        <div className='select' onClick={() => setSelect(9)}>
                             <h1>창업 동아리</h1>
                             <img src='https://cdn.pixabay.com/photo/2016/11/29/05/45/astronomy-1867616_960_720.jpg' width={500} />
                         </div>
-                        <div className='select'>
+                        <div className='select' onClick={() => setSelect(10)}>
                             <h1>coding bottle 활동</h1>
                             <img src='https://cdn.pixabay.com/photo/2016/11/29/05/45/astronomy-1867616_960_720.jpg' width={500} />
                         </div>
-                        <div className='select'>
+                        <div className='select' onClick={() => setSelect(11)}>
                             <h1>캘린더 제작 프로젝트</h1>
                             <img src='https://cdn.pixabay.com/photo/2016/11/29/05/45/astronomy-1867616_960_720.jpg' width={500} />
                         </div>
-                        <div className='select'>채우자</div>
+                        <div className='select' onClick={() => setSelect(12)}>채우자</div>
                     </div>
                     <div className='firstcontent'>
-                        <div className='select'>채우자</div>
-                        <div className='select'>채우자</div>
-                        <div className='select'>
+                        <div className='select' onClick={() => setSelect(13)}>채우자</div>
+                        <div className='select' onClick={() => setSelect(14)}>채우자</div>
+                        <div className='select' onClick={() => setSelect(15)}>
                             <h1>내가 했던 활동</h1>
                             <img src='https://cdn.pixabay.com/photo/2016/11/29/05/45/astronomy-1867616_960_720.jpg' width={500} />
                         </div>
-                        <div className='select'>
+                        <div className='select' onClick={() => setSelect(16)}>
                             <h1>내가 들었던 수업</h1>
                             <img src='https://cdn.pixabay.com/photo/2016/11/29/05/45/astronomy-1867616_960_720.jpg' width={500} />
                         </div>
